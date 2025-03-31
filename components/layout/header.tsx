ba4337e41a3541ca8d22e2e860f9af69aa00502e
Nicolas Dutertre <nicolasdutertre@MacBook-Air-de-Nicolas.local>
2025-03-31 11:05:13 +0200

    header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,10 +1,21 @@
 import { ModeToggle } from "../theme/theme-mode-toggle";
+import Link from "next/link";
 
 export function Header() {
     return (
         <header className="flex items-center justify-between px-4 py-2 border-b border-accent">
             <p className="font-extrabold text-xl">NextJS template</p>
-            <div className="flex-1"></div>
+            <nav className="flex-1 flex justify-center gap-8">
+                <Link href="/" className="hover:text-primary transition-colors">
+                    Accueil
+                </Link>
+                <Link href="/a-propos" className="hover:text-primary transition-colors">
+                    À propos
+                </Link>
+                <Link href="/contact" className="hover:text-primary transition-colors">
+                    Contact
+                </Link>
+            </nav>
             <ModeToggle />
         </header>
     )
